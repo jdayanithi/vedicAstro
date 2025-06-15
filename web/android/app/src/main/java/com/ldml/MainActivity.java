94ae4e434904cc84218be3d1897e17545ce25d97
jdayanithi <jdayanithi@gmail.com>
2025-06-15 21:07:58 +0530

    Angular upgrade

---
+++ web/android/app/src/main/java/com/ldml/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ldml;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vedicastrology.mobile',
+  appName: 'Vedic Astrology',
+  webDir: 'dist/browser'
+};
+
+export default config;
