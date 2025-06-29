71c28fed626efe8df79173c8ec87518d22abd3c8
jdayanithi <jdayanithi@gmail.com>
2025-06-29 18:59:10 +0530

    good shape

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://10.0.2.2:8080/api', // Use 10.0.2.2 for Android emulator to access host machine
+  apiUrl: 'http://localhost:8080/api', // Use localhost for web development
   googleClientId: '303692874838-i7u06gh4rchrjgckp1c53m4lqma8btlo.apps.googleusercontent.com' // Replace with your actual Google Client ID
 };
