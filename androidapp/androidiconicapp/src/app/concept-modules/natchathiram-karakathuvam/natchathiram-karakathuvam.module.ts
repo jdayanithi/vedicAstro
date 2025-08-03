9e82bcdf8749746475d7930c42744594eef8d284
jdayanithi <jdayanithi@gmail.com>
2025-08-03 20:30:38 +0530

    Kiraga serkai

--- androidapp/androidiconicapp/src/app/concept-modules/natchathiram-karakathuvam/natchathiram-karakathuvam.module.ts
+++ androidapp/androidiconicapp/src/app/concept-modules/natchathiram-karakathuvam/natchathiram-karakathuvam.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,6 +13,7 @@ import { NatchathiramKarakathuvamPage } from './natchathiram-karakathuvam.page';
   imports: [
     CommonModule,
     FormsModule,
+    HttpClientModule,
     IonicModule,
     NatchathiramKarakathuvamPageRoutingModule
   ],
